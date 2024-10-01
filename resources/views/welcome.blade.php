a66aeef3a05f78b854414f3e394d6cd99ada0f1d
Roberto Russo <roberto.russo@aulab.it>
2024-10-01 11:08:39 +0200

    inizio deploy

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <header class="min-vh-100">
+        <img src="https://th.bing.com/th/id/R.646c1ec8d53c75eb85ef6eddda734c51?rik=okkJjNkmwPlyYQ&riu=http%3a%2f%2fgetwallpapers.com%2fwallpaper%2ffull%2fb%2f8%2ff%2f282452.jpg&ehk=OfIIlK%2fuGsrgKMUzcybMR71r7uPMREQup0d1YQbYtw8%3d&risl=&pid=ImgRaw&r=0" alt="">
+    </header>
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1 class="h1-custom-color">Messa online della 152</h1>
+            </div>
+        </div>
+    </div>
+</x-layout>
